e609e5160eaf86db2cdc2a9d5513929175c7e5ba
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2026-01-09 11:21:06 +0100

    docs: Comments

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,31 +1,33 @@
-import * as dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import express from 'express';
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
 
 dotenv.config();
 
-const app = express();
+const app = express(); //Initialize Express Server
 const port = process.env.PORT || 3000;
 
 const connectDB = async () => {
-    const mongoUri = process.env.MONGO_URI;
+  const mongoUri = process.env.MONGO_URI;
 
-    if (!mongoUri) {
-        console.error("❌ Fehler: MONGO_URI ist nicht in der .env-Datei gesetzt.");
-        return;
-    }
+  if (!mongoUri) {
+    console.error("❌ Fehler: MONGO_URI ist nicht in der .env-Datei gesetzt.");
+    return;
+  }
 
-    try {
-        await mongoose.connect(mongoUri);
-        console.log('✅ MongoDB erfolgreich verbunden!');
-
-        app.listen(port, () => {
-            console.log(`🚀 Server läuft auf Port ${port}`);
-        });
-
-    } catch (error) {
-        console.error('❌ MongoDB Verbindungsfehler. Ist der Docker-Container gestartet? \n Fehler-Details:', error);
-    }
+  try {
+    await mongoose.connect(mongoUri); //Establish database connection
+    console.log("✅ MongoDB erfolgreich verbunden!");
+    //Start server
+    app.listen(port, () => {
+      console.log(`🚀 Server läuft auf Port ${port}`);
+    });
+  } catch (error) {
+    console.error(
+      "❌ MongoDB Verbindungsfehler. Ist der Docker-Container gestartet? \n Fehler-Details:",
+      error
+    );
+  }
 };
 
 connectDB();
