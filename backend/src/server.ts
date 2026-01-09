7ebe6a80625dd55d3880489d05fd1a04ea73b537
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2026-01-09 11:08:04 +0100

    feat: .env and server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,31 @@
+import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import express from 'express';
 
-const app = express();
-const port = 3000;
+dotenv.config();
 
-app.listen(port, () => {
-    console.log(`🎉 Server läuft auf http://localhost:${port}. Backend ist bereit!`);
-});
+const app = express();
+const port = process.env.PORT || 3000;
+
+const connectDB = async () => {
+    const mongoUri = process.env.MONGO_URI;
+
+    if (!mongoUri) {
+        console.error("❌ Fehler: MONGO_URI ist nicht in der .env-Datei gesetzt.");
+        return;
+    }
+
+    try {
+        await mongoose.connect(mongoUri);
+        console.log('✅ MongoDB erfolgreich verbunden!');
+
+        app.listen(port, () => {
+            console.log(`🚀 Server läuft auf Port ${port}`);
+        });
+
+    } catch (error) {
+        console.error('❌ MongoDB Verbindungsfehler. Ist der Docker-Container gestartet? \n Fehler-Details:', error);
+    }
+};
+
+connectDB();
