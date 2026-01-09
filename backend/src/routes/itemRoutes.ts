fc5573248673a1cd9f59ce37926f5b73cb5559ae
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2026-01-09 12:51:39 +0100

    feat: Create Item Router

---
+++ backend/src/routes/itemRoutes.ts
@@ -0,0 +1,23 @@
+import express, { Router } from 'express';
+import {
+  getAllItems,
+  createItem,
+  updateItemStatus,
+  deleteItem,
+} from '../controllers/itemController';
+
+const router: Router = express.Router();
+
+// get all items
+router.get('/', getAllItems);
+
+// Add a new item
+router.post('/', createItem);
+
+// Update the status of an item
+router.put('/:id', updateItemStatus);
+
+// Delete an item
+router.delete('/:id', deleteItem);
+
+export default router;
